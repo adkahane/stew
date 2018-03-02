4a9e415f7b4034f085d5d821de57322ccc522067
adkahane <adam.kahane.nj@gmail.com>
2018-03-01 20:04:12 -0800

    orm set up

--- config/orm.js
+++ config/orm.js
@@ -1,15 +1,85 @@
 var connection = require("../config/connection.js");
 
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i <num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+// Helper function
+function objToSql(ob) {
+  var arr = [];
+
+  // Loop through the keys and push the key/value as a string into arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if has spaces, add quotations around string
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
+}
+
 var orm = {
-  selectAll: function() {
-
+  // Select all from a table
+  selectAll: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
   },
-  insertOne: function() {
+  // Insert into a table
+  insertOne: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
 
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
   },
-  updateOne: function() {
+  // Update a table
+  updateOne: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
 
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
   }
 };
 
+// Export for cat.js
 module.exports = orm;
