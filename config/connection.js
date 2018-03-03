998317f69ef44fedaf098a449d0c0d138dd0b70f
adkahane <adam.kahane.nj@gmail.com>
2018-03-02 20:14:39 -0800

    deploy to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // SQL connection
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "iamR00t",
-  database: "stew_db"
-});
+if(process.env.JAWSDB_URL) {
+  // Heroku deployment
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "iamR00t",
+    database: "stew_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
